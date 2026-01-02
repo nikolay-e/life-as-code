b810f2fe4808b2acb78101666f2b0d526e729c67
Nikolay Eremeev <nikolay.eremeev@outlook.com>
2026-01-02 17:02:06 +0100

    Fix SQLAlchemy DetachedInstanceError in get_user_credentials

--- src/utils.py
+++ src/utils.py
@@ -6,6 +6,9 @@ from models import UserCredentials
 
 def get_user_credentials(user_id: int) -> UserCredentials | None:
     with get_db_session_context() as db:
-        return db.scalars(
+        creds = db.scalars(
             select(UserCredentials).where(UserCredentials.user_id == user_id)
         ).first()
+        if creds:
+            db.expunge(creds)
+        return creds
